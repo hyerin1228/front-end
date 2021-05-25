ba482392dbd5731f3c24aea69de30f497f2ee1e4
bosees <rladuddlf3@naver.com>
2021-05-25 11:17:39 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Switch, Route} from "react-router-dom";
-import styles from "./app.module.css"; //postCSS 입니다. CSS는 셀렉터(클래스네임,아이디네임) 값이 
+import styles from "./App.module.css"; //postCSS 입니다. CSS는 셀렉터(클래스네임,아이디네임) 값이 
                                        //겹치지 않게 사용하는게 일반적인데 postCSS를 사용함으로써 moduling을 할수 있습니다.
                                        //즉 각 컴포넌트마다 독립적으로 css효과를 줄수있기 때문에 협업시 이름이 겹칠 걱정을 할 필요가 없어집니다.
 import Navbar from "./components/navbar/navbar";
